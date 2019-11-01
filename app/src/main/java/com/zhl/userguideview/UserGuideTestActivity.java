c3aeb3e49228644c5c87114ecb4f8b50d6dc50d8
yilylong <yilylong@163.com>
2019-11-01 16:44:46 +0800

    修改一个右下角箭头位置计算的bug

--- app/src/main/java/com/zhl/userguideview/UserGuideTestActivity.java
+++ app/src/main/java/com/zhl/userguideview/UserGuideTestActivity.java
@@ -20,12 +20,13 @@ import java.util.LinkedHashMap;
  * Created by zhaohl on 2015-11-26.
  */
 public class UserGuideTestActivity extends Activity {
-    private String[] datas = new String[]{"收藏","字体大小","软件设置","换肤"};
+    private String[] datas = new String[]{"收藏", "字体大小", "软件设置", "换肤"};
     GridView mGridView;
     private UserGuideView guideView;
-    private ImageView icon ,back,top;
+    private ImageView icon, back, top, bottomLeft, bottomRight;
     private CBPullRefreshListView listView;
     View tipTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,7 +34,7 @@ public class UserGuideTestActivity extends Activity {
         guideView = (UserGuideView) findViewById(R.id.guideView);
         guideView.setTouchOutsideDismiss(false);
 //        guideView.setShowArrow(false);
-        tipTextView = LayoutInflater.from(this).inflate(R.layout.custom_tipview,null);
+        tipTextView = LayoutInflater.from(this).inflate(R.layout.custom_tipview, null);
 //        icon = (ImageView) findViewById(R.id.icon);
 //        guideView.setHighLightView(icon);
 //        guideView.setTipView(tipTextView);
@@ -42,17 +43,19 @@ public class UserGuideTestActivity extends Activity {
         icon = (ImageView) findViewById(R.id.icon);
         back = (ImageView) findViewById(R.id.back);
         top = (ImageView) findViewById(R.id.top);
+        bottomLeft = (ImageView) findViewById(R.id.bottomleft);
+        bottomRight = (ImageView) findViewById(R.id.bottomRight);
         icon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                guideView.setTipView(tipTextView,400,200);
+                guideView.setTipView(tipTextView, 400, 200);
                 guideView.setHighLightView(icon);
             }
         });
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                guideView.setTipView(tipTextView,400,200);
+                guideView.setTipView(tipTextView, 400, 200);
                 guideView.setHighLightView(back);
             }
         });
@@ -64,12 +67,18 @@ public class UserGuideTestActivity extends Activity {
                 guideView.setHighLightView(top);
             }
         });
-        LinkedHashMap<View,Integer> targets = new LinkedHashMap<>();
-        targets.put(top,R.mipmap.panda);
-        targets.put(icon,R.mipmap.tip2);
-        targets.put(back,R.mipmap.tip3);
+        LinkedHashMap<View, Integer> targets = new LinkedHashMap<>();
+        targets.put(top, R.mipmap.panda);
+        targets.put(icon, R.mipmap.tip2);
+        targets.put(back, R.mipmap.tip3);
+        targets.put(bottomLeft, R.mipmap.tip3);
+        targets.put(bottomRight, R.mipmap.tip3);
 //        guideView.setHighLightView(top,icon,back);
+        guideView.setArrowDownRight(R.mipmap.guide_arrow_right);
+        guideView.setArrowDownCenter(R.mipmap.guide_arrow_right);
+        guideView.setArrowDownLeft(R.mipmap.guide_arrow_left);
         guideView.setArrowUpLeftMoveX(-30);
+//        guideView.setArrowDownRightMoveX(80);
 //        guideView.setArrowDownCenterMoveX(100);
 //        guideView.setTipViewMoveX(icon,-100);
 //        guideView.setTipViewMoveY(icon,100);
@@ -96,24 +105,24 @@ public class UserGuideTestActivity extends Activity {
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             ViewHolder viewHolder = null;
-           if(convertView==null){
-               viewHolder = new ViewHolder();
-               convertView = LayoutInflater.from(UserGuideTestActivity.this).inflate(R.layout.grid_item,parent,false);
-               viewHolder.textView = (TextView) convertView.findViewById(R.id.tx);
-               convertView.setTag(viewHolder);
-           }else{
-               viewHolder = (ViewHolder) convertView.getTag();
-           }
+            if (convertView == null) {
+                viewHolder = new ViewHolder();
+                convertView = LayoutInflater.from(UserGuideTestActivity.this).inflate(R.layout.grid_item, parent, false);
+                viewHolder.textView = (TextView) convertView.findViewById(R.id.tx);
+                convertView.setTag(viewHolder);
+            } else {
+                viewHolder = (ViewHolder) convertView.getTag();
+            }
             viewHolder.textView.setText(datas[position]);
 //            if(position==1){
 ////                guideView.setTipView(BitmapFactory.decodeResource(getResources(),R.mipmap.sidebar_photo));
 //                guideView.setHighLightView(convertView);
 //            }
-           return convertView;
+            return convertView;
         }
 
-        private class ViewHolder{
-           public TextView textView;
+        private class ViewHolder {
+            public TextView textView;
         }
     }
 }
